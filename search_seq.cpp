538a4fe5d5a1e91060b5ab91c1ef4d78de9bff66
Balaji Narayanaswami <balajinandhu@gmail.com>
2015-10-02 21:38:08 -0700

    sequential search for boyer moore algorithm

---
+++ search_seq.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+#include<fstream>
+#include<cstring>
+#include<cstdlib>
+#include<string>
+#include<assert.h>
+#include<iterator>
+#include<algorithm>
+#include<sstream>
+#include "boost/algorithm/searching/boyer_moore.hpp"
+#include "boost/algorithm/string.hpp"
+
+using namespace std;
+using namespace boost;
+using namespace boost::algorithm;
+
+int main(int argc, char*argv[]){
+
+	//read the text file from local drive
+	ifstream in("haystack.txt");
+
+	//store in string object
+	string contents((istreambuf_iterator<char>(in)),
+		istreambuf_iterator<char>());
+
+	string contents_copy = contents;
+	erase_all(contents_copy, " ");
+	erase_all(contents_copy, "\n");
+	erase_all(contents_copy, "\t");
+	erase_all(contents_copy, "\r");
+	erase_all(contents_copy, "\v");
+	string pattern = argv[1];//"England";//"PoliticalEconomy"; //"couldn'tstandit";
+
+    string::const_iterator beg  = contents.begin();
+    string::const_iterator it_beg  = contents_copy.begin();
+    string::const_iterator it_s  = contents_copy.begin();
+    string::const_iterator ite_s = contents_copy.end();
+    
+    vector<int> line_nos;
+
+    do{
+    string::const_iterator it_res = boyer_moore_search<>(
+             it_s, ite_s,				
+             pattern.begin(),
+             pattern.end());
+	
+    //to convert compressed text iterator to normal text iterator, 
+    //calculate offset of the number of chars from beg to it_res 
+    unsigned offset = std::distance(it_beg, it_res);
+    //move offset chars in contents ignoring whitespace - get iterator to normal text
+    string::const_iterator il = contents.begin();
+    int wcount=0;
+    for(int j=0;j<=offset+wcount;++j) isspace(*il)? wcount++,*il++:*il++;
+
+	int line_count = std::count(beg,il, '\n');
+	line_nos.push_back(line_count+1);
+	advance(it_res, pattern.length());
+	it_s = it_res;
+
+	}while(it_s<ite_s);
+
+	in.close();
+	
+	//print number of occurences
+	cout<<"Number of occurences of pattern in text: "<<line_nos.size()-1<<endl;
+	//print line numbers 
+	cout<<"The line numbers where the pattern occurs are: "<<endl;
+	for(vector<int>::iterator itr = line_nos.begin();itr<line_nos.end()-1;++itr){
+		cout<<*itr<<endl;
+	
+	}
+
+	return 1;
+}
